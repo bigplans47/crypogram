d7490b2b8e3ec05715be0da1a9edaa3fb6184af5
Austin Cummings and Daniel Kiss <austin@>
2017-08-17 16:14:31 -0700

    add way to put code into arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,54 @@
 var cleanSentence = function(inputFromUser) {
   var input1 = inputFromUser;
-var string = input1.replace(/[^\s\w]|_/g, '');
+var string = input1.replace(/\s+/g, '');
+var stringArray=string.split();
 alert("this is firing " +string);
+var howLong = parseInt(string.length);
+var dimensions = Math.ceil(Math.sqrt(howLong));
+
+
+
+
+// alert(stringArray+ " this is what we have");
+// for (var i = 0; i < dimensions; i++) {
+//   testarray = string.slice(dimensions*i, (dimensions*(i+1)) );
+// }
+
+alert(dimensions);
+alert(string+ " this is what we have left" )  // we split strings into groups of dimension size
+
+// masterArray=[];
+// index1=0;
+// index2=dimensions;
+// for (i = 0; i<=10 ; i++) {
+// masterArray[i]= string.slice(index1,index2);
+// index1=index1+dimensions;
+// index2=index2+dimensions;
+// // index1=index1+8
+// // index2=index2+9
+// console.log(masterArray[i]);
+// }
+// dont compare yourself to others compare yourself to the person you were yesterday
+
+
+masterArray=[];
+index1=0;
+index2=dimensions;
+for (i=0; i<=dimensions; i++) {
+  masterArray[i] = string.slice(index1,index2);
+  index1=index1+dimensions-1;
+  index2=index2+dimensions-1;
+  console.log(masterArray[i]);
+}
+
+
+
+
+console.log(masterArray);
+
+
+
 return string;
-
-
 }
 
 $(document).ready(function() {
@@ -13,7 +57,6 @@ $(document).ready(function() {
     var inputFromUser = $("#message").val();
     var output = cleanSentence(inputFromUser);
 $("#result").append("<P>" +output+ "</p>");
-    // $("#result").text(string2);
 
 
 
